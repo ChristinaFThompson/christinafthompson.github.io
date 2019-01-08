f24a602ca14cfdae1bf7bbfd4b850f40c9805352
“Christina <cfthompson321@gmail.com>
2019-01-07 21:23:15 -0500

    Updates

---
+++ precache-manifest.73d32acd889e97ec810d6fac73ee629c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8614dcd04d964ff4c789",
+    "url": "/static/css/main.47c13f58.chunk.css"
+  },
+  {
+    "revision": "8614dcd04d964ff4c789",
+    "url": "/static/js/main.8614dcd0.chunk.js"
+  },
+  {
+    "revision": "f230d321eceaffdef15a",
+    "url": "/static/js/1.f230d321.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d02b2647d0ce9930b9d60147edd9633c",
+    "url": "/index.html"
+  }
+];
