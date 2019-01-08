5a68cda0deba74bfcc7561a554145e2ccffee1f1
“Christina <cfthompson321@gmail.com>
2019-01-07 21:16:09 -0500

    Updates

---
+++ precache-manifest.f0cbb5c291d08d0bc069b3604f016c6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc248ac9ff1745fabce",
+    "url": "/christinafthompson.github.io.git/static/css/main.47c13f58.chunk.css"
+  },
+  {
+    "revision": "ccc248ac9ff1745fabce",
+    "url": "/christinafthompson.github.io.git/static/js/main.ccc248ac.chunk.js"
+  },
+  {
+    "revision": "f230d321eceaffdef15a",
+    "url": "/christinafthompson.github.io.git/static/js/1.f230d321.chunk.js"
+  },
+  {
+    "revision": "a93ca46dac728d836d59",
+    "url": "/christinafthompson.github.io.git/static/js/runtime~main.a93ca46d.js"
+  },
+  {
+    "revision": "257f5f47e0a5794764a94faaece07ee7",
+    "url": "/christinafthompson.github.io.git/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73d32acd889e97ec810d6fac73ee629c.js"
+  "/christinafthompson.github.io.git/precache-manifest.f0cbb5c291d08d0bc069b3604f016c6c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/christinafthompson.github.io.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
